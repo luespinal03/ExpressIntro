623639371ac90b6293bd05e67b9473615c9216ff
Luis Espinal <luisespinal294@gmail.com>
2022-09-06 13:03:44 -0400

    ExpressIntro

--- app.js
+++ app.js
@@ -4,16 +4,24 @@ const express = require('express')
 // initialize the Server and Port
 const app = express()
 const port = 3000
+const favoriteMovieList = ["Star Wars", "The Avengers", "A random movie"]
+let movieString = favoriteMovieList.join(', ')
+const today = new Date()
 
 // Define the default server route (aka "/") for our server
 app.get('/', (req, res) => {
     console.log("Default Route")
-    res.send('Hello World!')
+    res.send(`My name is Luis Espinal. Today is: ${today}`)
 })
 
 app.get('/hello-class', (req, res) => {
     console.log("Hello Class Route")
     res.send('Hello Class!')
+})
+
+app.get('/favorite-movies', (req, res) => {
+    console.log("Favorite Movie Route")
+    res.send(`${movieString}`)
 })
 // Finally, run the server
 app.listen(port, () => {
