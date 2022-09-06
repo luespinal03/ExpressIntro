50ac73337d7e6173f5a161c8235acafa2f67c73c
Luis Espinal <luisespinal294@gmail.com>
2022-09-06 12:25:42 -0400

    ExpressIntro

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // look for a file in node_modules called express and give variable express the value of all that code in file express
 const express = require('express')
 
+// initialize the Server and Port
 const app = express()
 const port = 3000
 
+// Define the default server route (aka "/") for our server
 app.get('/', (req, res) => {
     console.log("Default Route")
     res.send('Hello World!')
 })
 
+app.get('/hello-class', (req, res) => {
+    console.log("Hello Class Route")
+    res.send('Hello Class!')
+})
+// Finally, run the server
 app.listen(port, () => {
+    // Console.log app listening on port when the server is running
     console.log(`Example app listening on port ${port}`)
 })
