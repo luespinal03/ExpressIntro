6f55884a21146711cc343d6fe7504a82c241f8f3
Luis Espinal <luisespinal294@gmail.com>
2022-09-06 12:07:04 -0400

    ExpressIntro

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// look for a file in node_modules called express and give variable express the value of all that code in file express
 const express = require('express')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    console.log("Default Route")
     res.send('Hello World!')
 })
 
